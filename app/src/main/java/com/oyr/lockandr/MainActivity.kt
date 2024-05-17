a1cabafdf0eca5a31fb7b905b33e26466ef3c3ec
onatyr <onat.rigault@gmail.com>
2024-05-17 23:33:02 +0200

    app show icon and label

--- app/src/main/java/com/oyr/lockandr/MainActivity.kt
+++ app/src/main/java/com/oyr/lockandr/MainActivity.kt
@@ -1,26 +1,33 @@
 package com.oyr.lockandr
 
-import android.app.PendingIntent
 import android.app.admin.DevicePolicyManager
-import android.content.BroadcastReceiver
 import android.content.ComponentName
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.content.pm.PackageInstaller
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.oyr.lockandr.ui.theme.LockAndRTheme
 
 
@@ -34,38 +41,60 @@ class MainActivity : ComponentActivity() {
         getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val pm = packageManager
 
-        val makeHidden = !devicePolicyManager.isApplicationHidden(adminComponentName, "fr.onat68.aileronsappmapandroid")
-        devicePolicyManager.setApplicationHidden(adminComponentName, "fr.onat68.aileronsappmapandroid", makeHidden)
+        val appInfos = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            pm.getInstalledApplications(PackageManager.ApplicationInfoFlags.of(0L)).filter { it.isProfileable }
+        } else {
+            pm.getInstalledApplications(0)
+        }
+
+        val makeHidden = !devicePolicyManager.isApplicationHidden(
+            adminComponentName,
+            "fr.onat68.aileronsappmapandroid"
+        )
+        devicePolicyManager.setApplicationHidden(
+            adminComponentName,
+            "fr.onat68.aileronsappmapandroid",
+            makeHidden
+        )
         enableEdgeToEdge()
         setContent {
             LockAndRTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    LazyColumn {
+                        items(appInfos) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                modifier = Modifier.padding(horizontal = 10.dp)
+                            ) {
+                                Image(
+                                    bitmap = drawableToBitmap(it.loadIcon(pm)).asImageBitmap(),
+                                    contentDescription = "app icon",
+                                    modifier = Modifier.size(50.dp)
+                                )
+                                Text(text = it.loadLabel(pm).toString())
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun drawableToBitmap(drawable: Drawable): Bitmap {
+    val bitmap = Bitmap.createBitmap(
+        drawable.intrinsicWidth,
+        drawable.intrinsicHeight,
+        Bitmap.Config.ARGB_8888
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LockAndRTheme {
-        Greeting("Android")
-    }
+    val canvas = Canvas(bitmap)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+    return bitmap
 }
 
